d67b4ab0471097ba2017512d40bf759f6e12832f
Christopher Duggan <christopherduggan@gmail.com>
2019-05-13 18:32:26 -0400

    progress on routes, problems with updating product list

--- routes/vendors.js
+++ routes/vendors.js
@@ -3,28 +3,52 @@ const { Vendor } = require('../models');
 
 const vendorRouter = Router();
 
-//create new vendor
-vendorRouter.post('/vendors/', async (request, response, next) => {
-    try{
-        const vendor = await Vendor.create(req.body);
-        res.json({ vendor });
-    } catch (e){
-        next(e);
-    }
-};
+// create new vendor
+vendorRouter.post('/', async (request, response, next) => {
+  try {
+    const vendor = await Vendor.create(request.body);
+    response.json({ vendor });
+  } catch (e) {
+    next(e);
+  }
+});
 
-//update vendor
-vendorRouter.put('/vendors/:id', async (request, response, next) => {
-    try {
-        const { id } = request.params;
-        const vendor = await Vendor.findByPk(id);
-        vendor.update(request.body);
-        response.json({ vendor });
-    } catch (e) {
-        next(e);
-    }
-}
+// update vendor
+vendorRouter.put('/:id', async (request, response, next) => {
+  try {
+    const { id } = request.params;
+    const vendor = await Vendor.findByPk(id);
+    vendor.update(request.body);
+    response.json({ vendor });
+  } catch (e) {
+    next(e);
+  }
+});
 
+// update vendor products
+//vendorRouter.put('/:id/products', async (request, response, next) => {
+//  try {
+//    const { id } = request.params;
+//    const vendor = await Vendor.findByPk(id);
+//    vendor.update(vendor.dataValues.products +=`, ${request.body.products}`);
+//    console.log(vendor);
+//    response.json({ vendor });
+//  } catch (e) {
+//    console.log(e);
+//    next(e);
+//  }
+// });
+
+//
+vendorRouter.get('/', async (request, response, next) => {
+  try {
+    const vendors = await Vendor.findAll();
+
+    response.json({ vendors });
+  } catch (e) {
+    next(e);
+  }
+});
 
 
 //****************************** POST MVP **********************************
@@ -39,4 +63,4 @@ vendorRouter.put('/vendors/:id', async (request, response, next) => {
 
 
 
-module.exports = vendorRouter
+module.exports = vendorRouter;
