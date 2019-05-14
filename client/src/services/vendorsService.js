87e8593a20dbcfa1063e8d3eacb60ce3cd522515
ameliadz <ameliazecker@gmail.com>
2019-05-14 15:28:49 -0400

    adding services folder

---
+++ client/src/services/vendorsService.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:3001';
+
+export const getVendors = async () => {
+  const vendors = await axios.get(`${BASE_URL}/vendors`);
+  return vendors.data.vendors;
+};
+
+export const createVendor = async () => {
+  const vendor = await axios.post(`${BASE_URL}/vendors`);
+  console.log(vendor);
+}
