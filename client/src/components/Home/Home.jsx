29b3a848c75222be768c7d60e527779a4dd7ce49
Rachel Gonzalez <dancearchitect@gmail.com>
2019-05-14 09:51:18 -0400

    added search container for real

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-
+import SearchContainer from "../SearchContainer/SearchContainer";
 
 class Home extends Component {
   render() {
     return (
       <div>
         <h1>GreenMark!t</h1>
+        <SearchContainer />
         <h2>For Vendors, Made by Nerds</h2>
         <h3>About</h3>
         <p>
