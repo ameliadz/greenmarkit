cc50e6ddeb40fe7d138bbfd2d662eae8ff80f944
Ryan Crawford <ryancrawfordny@gmail.com>
2019-05-16 17:37:29 -0400

    need to pull new. Some styling fixed

--- client/src/components/VendorInfoForm/VendorDaysFilter.jsx
+++ client/src/components/VendorInfoForm/VendorDaysFilter.jsx
@@ -1,25 +1,56 @@
 import React, { Component } from "react";
+import './VendorInfoForm.css';
 
 class VendorDaysFilter extends Component {
   render() {
     return (
       <div className="days-filter">
-        <label className="open" htmlFor="days"><strong>Days Open</strong></label>
-        <div>
-          <input type="checkbox" name="days" id="monday" value="1" onChange={this.props.handleDaySelect} />
-          <label htmlFor="monday">Monday</label>
-        </div>
-        <div>
-          <input type="checkbox" name="days" id="wednesday" value="2" onChange={this.props.handleDaySelect}/>
-          <label htmlFor="wednesday">Wednesday</label>
-        </div>
-        <div>
-          <input type="checkbox" name="days" id="friday" value="3" onChange={this.props.handleDaySelect}/>
-          <label htmlFor="friday">Friday</label>
-        </div>
-        <div>
-          <input type="checkbox" name="days" id="saturday" value="4" onChange={this.props.handleDaySelect}/>
-          <label htmlFor="saturday">Saturday</label>
+        <div className="row">
+          <div className="column">
+            <label className="open" htmlFor="days">
+              <strong>Days Open</strong>
+            </label>
+            <div>
+              <input
+                type="checkbox"
+                name="days"
+                id="monday"
+                value="1"
+                onChange={this.props.handleDaySelect}
+              />
+              <label htmlFor="monday">Monday</label>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                name="days"
+                id="wednesday"
+                value="2"
+                onChange={this.props.handleDaySelect}
+              />
+              <label htmlFor="wednesday">Wednesday</label>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                name="days"
+                id="friday"
+                value="3"
+                onChange={this.props.handleDaySelect}
+              />
+              <label htmlFor="friday">Friday</label>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                name="days"
+                id="saturday"
+                value="4"
+                onChange={this.props.handleDaySelect}
+              />
+              <label htmlFor="saturday">Saturday</label>
+            </div>
+          </div>
         </div>
       </div>
     );
