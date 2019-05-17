16baa10e4b8c069d45599972d6b23fcee917ed79
Rachel Gonzalez <dancearchitect@gmail.com>
2019-05-17 09:14:21 -0400

    safety commit

--- client/src/components/AddProductEntry/AddProductEntry.jsx
+++ client/src/components/AddProductEntry/AddProductEntry.jsx
@@ -1,29 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 function AddProductEntry(props) {
-  if (props.addEntry === true ) {
+  if (props.addEntry === true) {
     return (
       <div>
-        <form onSubmit={(e) => {
-          e.preventDefault();
-          props.setProducts({products: props.state.products + ', ' + props.state.newProduct});
-          props.renderProductList();
-          props.handleAdd();
-        }}> 
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            props.setProducts({
+              products: props.state.products + ", " + props.state.newProduct
+            });
+            props.renderProductList();
+            props.handleAdd();
+          }}
+        >
           <label>Enter Product</label>
-          <input 
+          <input
             onChange={props.handleTextInput}
-            type="text" 
-            name="newProduct" 
+            type="text"
+            name="newProduct"
             value={props.state.newProduct}
           />
-          <input 
-            type="submit" 
-            value="Add"
-          />
+          <input type="submit" value="Add" />
         </form>
       </div>
-    )
+    );
   } else {
     return null;
   }
