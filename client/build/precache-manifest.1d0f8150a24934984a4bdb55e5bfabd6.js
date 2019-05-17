28c41ccec1b70cc23ee7f7d851646ba05f2215ba
ameliadz <ameliazecker@gmail.com>
2019-05-17 16:48:08 -0400

    another try to deploy

---
+++ client/build/precache-manifest.1d0f8150a24934984a4bdb55e5bfabd6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa11cc93b81b69ac20a609c700b4c81b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bcb8a81e77c7a845a60",
+    "url": "/static/css/main.41c0ae3c.chunk.css"
+  },
+  {
+    "revision": "9dddb8d34d7504205851",
+    "url": "/static/js/2.fd9c6dda.chunk.js"
+  },
+  {
+    "revision": "6bcb8a81e77c7a845a60",
+    "url": "/static/js/main.051990d4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2bb0e578e0b639d7ab8e31f07e67e2cd",
+    "url": "/static/media/friday-map.2bb0e578.png"
+  },
+  {
+    "revision": "60dbb420f9a112dcb48592c16acdc211",
+    "url": "/static/media/markit-logo.60dbb420.png"
+  },
+  {
+    "revision": "ec1c46ea6aef8b3d09ae0dd3f0ba1ce2",
+    "url": "/static/media/monday-map.ec1c46ea.png"
+  },
+  {
+    "revision": "deb76256f195ee5a0c7032319e209790",
+    "url": "/static/media/saturday-map.deb76256.png"
+  },
+  {
+    "revision": "386e8f864b9cd0e8385b8a31aacc5f2e",
+    "url": "/static/media/wednesday-map.386e8f86.png"
+  }
+]);
