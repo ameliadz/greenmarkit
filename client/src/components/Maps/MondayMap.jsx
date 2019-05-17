f1ead4a02dafbdc2bd5b6adced56984309785a76
Rachel Gonzalez <dancearchitect@gmail.com>
2019-05-16 21:35:55 -0400

    styled maps and edited logo

--- client/src/components/Maps/MondayMap.jsx
+++ client/src/components/Maps/MondayMap.jsx
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
-import fridayMap from "../Maps/map-boilerplate-friday.png";
+import mondayMap from "../Maps/monday-map.png";
 
 class FridayMap extends Component {
+  constructor() {
+    super();
+    this.state = {
+      backgroundImage: `url(${mondayMap})`,
+      backgroundPosition: "0% 0%"
+    };
+  }
+
+  handleMouseMove = e => {
+    const { left, top, width, height } = e.target.getBoundingClientRect();
+    const x = ((e.pageX - left) / width) * 100;
+    const y = ((e.pageY - top) / height) * 100;
+    this.setState({ backgroundPosition: `${x}% ${y}%` });
+  };
   render() {
     return (
       <div>
         <div className="map-label">
-          <label for="monday">MONDAY</label>
-          <img src={fridayMap} alt="monday map" />
+          <label htmlFor="monday">MONDAY</label>
+          <figure onMouseMove={this.handleMouseMove} style={this.state}>
+            <img src={mondayMap} alt="monday map" />
+          </figure>
         </div>
       </div>
     );
